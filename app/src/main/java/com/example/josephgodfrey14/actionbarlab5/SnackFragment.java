34ab00c82b8dfd0458a63a7b5c0194948087a51a
Godfrey <JOSEPHGODFREY14@augustana.edu>
2018-03-19 15:13:00 -0500

    Initial commit

---
+++ app/src/main/java/com/example/josephgodfrey14/actionbarlab5/SnackFragment.java
@@ -0,0 +1,16 @@
+package com.example.josephgodfrey14.actionbarlab5;
+
+/**
+ * Created by josephgodfrey14 on 3/19/2018.
+ */
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.*;
+
+public class SnackFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_snack, container, false);
+    }
+}
